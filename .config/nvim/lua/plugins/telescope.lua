340a9109e178d825b2ef5f6bac4ccb70666209bd
reillyfeatherstone <contact@reillyfeatherstone.com>
2024-05-14 23:15:38 +0100

    Added LSP action window with <leader>cg

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+  {
     "nvim-telescope/telescope.nvim", tag = "0.1.6",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
@@ -8,4 +9,19 @@ return {
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extentions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
